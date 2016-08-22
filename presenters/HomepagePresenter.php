bba6c72831ec2e3e6ffd8fec0e42646c3e11d4cb
shadd0w <rene.gmitter@wame.sk>
2016-08-22 16:20:54 +0200

    UPDATE HomePage

--- presenters/HomepagePresenter.php
+++ presenters/HomepagePresenter.php
@@ -7,4 +7,9 @@ use App\Core\Presenters\BasePresenter;
 class HomepagePresenter extends BasePresenter
 {
     
+    public function renderDefault()
+    {
+        $this->template->siteTitle = _('Home');
+    }
+    
 }
