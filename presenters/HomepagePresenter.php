690d9120bb7987b023403dcc45540f1e2f770623
Stanislav Červeňák <stano@wame.sk>
2018-04-03 13:59:16 +0200

    FIX after upgrade PHP 7.2

--- presenters/HomepagePresenter.php
+++ presenters/HomepagePresenter.php
@@ -4,13 +4,9 @@ namespace App\HomepageModule\Presenters;
 
 use App\Core\Presenters\BasePresenter;
 
+
 class HomepagePresenter extends BasePresenter
 {
-    protected function startup()
-    {
-        parent::startup();
-    }
-    
     public function renderDefault()
     {
         $this->template->siteTitle = _('Homepage');
